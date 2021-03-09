1a01c25b383222664bed4a2a0071ad18b8124814
parkoon <devparkoon@gmail.com>
2021-03-09 23:43:46 +0900

    chore: build

--- build/passport-naver.js
+++ build/passport-naver.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Strategy = void 0;
-var NaverStrategy_1 = __importDefault(require("./NaverStrategy"));
-exports.Strategy = NaverStrategy_1.default;
-exports.default = NaverStrategy_1.default;
+var strategy_1 = __importDefault(require("./strategy"));
+exports.Strategy = strategy_1.default;
+exports.default = strategy_1.default;
